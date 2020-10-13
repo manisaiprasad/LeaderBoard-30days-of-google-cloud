da85c641a8dc7e3f68dfc8014278c07eeffcd7ae
manisaiprasadam <manisaiprasadam@gmail.com>
2020-10-13 17:42:20 +0530

    intial commit

---
+++ js/index.js
@@ -0,0 +1,57 @@
+console.clear();
+
+function getData() {
+    var xh = new XMLHttpRequest();
+    xh.open(
+        "GET",
+        "my.json",
+        true
+    );
+    xh.setRequestHeader("Content-Type", "application/json");
+    xh.send();
+    xh.onload = function () {
+        if (this.status == 200) {
+            var data = JSON.parse(this.responseText);
+            console.log(data);
+
+            var i = 1;
+            data.forEach(member => {
+                let newRow = document.createElement('div');
+                newRow.innerHTML = `
+                <div class="card-container">
+                    <img class="round" src="${member.dp}" alt="user" />
+                    <h3>Rank #${i}</h3>
+                    <p>${member.name}</p>
+                    <p>Quests Completed: ${member.qcomplete_no}</p>
+                    
+                    <div class="skills">
+                        <h6>Track 1</h6>
+                        <ul>
+                            <li>${member.track1}</li>
+                            
+                        </ul>
+                        <h6>Track 2</h6>
+                        <ul>
+                            <li>${member.track2}</li>
+                            
+                        </ul>
+                    </div>
+                    
+                </div>
+                <br>
+
+
+                `;
+              
+                i++;
+                list.appendChild(newRow);
+            });
+
+
+
+
+        } else {
+            console.log("Something went wrong.")
+        }
+    };
+}
